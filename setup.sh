96c8a1d0555cd055b43b83170f6076f3ecd50d4f
Tasibaev Davron <tdav@mail.ru>
2026-02-20 23:44:55 +0500

    feat(ofiq_rest_api): Containerize API with Docker and update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,11 +52,19 @@ echo "Setting executable permissions..."
 chmod +x "$CONSOLE_DIR/ikao"
 chmod +x "$API_DIR/OFIQ.RestApi"
 
+# Docker Build
+echo "Building Docker image for OFIQ REST API..."
+docker build -t ofiq-rest-api:latest .
+
 echo "------------------------------------------------"
 echo "Setup complete!"
 echo "Artifacts are located in the '$DIST_DIR' directory."
+echo "Docker image 'ofiq-rest-api:latest' has been created."
 echo ""
-echo "To run the Console App:"
+echo "To run the REST API in Docker:"
+echo "docker run -p 8080:8080 ofiq-rest-api:latest"
+echo ""
+echo "To run the Console App locally:"
 echo "cd $CONSOLE_DIR && ./ikao <path_to_image>"
 echo ""
 echo "To run the REST API:"
